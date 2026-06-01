06048bb4675fd7197b40d02bebe1b6327917e0d1
Debankan Roy <userx4006@gmail.com>
2026-06-01 21:21:46 +0530

    Initial Setup

---
+++ src/main/java/com/deba/bookstore/entity/Book.java
@@ -0,0 +1,79 @@
+package com.deba.bookstore.entity;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal; //for price, standard monitay values
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(unique = true, nullable = false)
+    private String isbn;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(name = "published_date")
+    private LocalDate publishedDate;
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDate getPublishedDate() {
+        return this.publishedDate;
+    }
+
+    public void setPublishedDate(LocalDate publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+}
