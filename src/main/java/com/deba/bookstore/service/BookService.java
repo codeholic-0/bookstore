9cc042d215fdb8a75648c071e06050f1abb14265
Debankan Roy <userx4006@gmail.com>
2026-06-02 00:08:47 +0530

    Added Repository, Controller, Service Layers

---
+++ src/main/java/com/deba/bookstore/service/BookService.java
@@ -0,0 +1,45 @@
+package com.deba.bookstore.service;
+
+import org.springframework.stereotype.Service;
+
+import com.deba.bookstore.repository.BookRepository;
+import com.deba.bookstore.entity.Book;
+
+import java.util.List;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getBookById(Long id) throws RuntimeException {
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found with id: " + id));
+    }
+
+    public Book updateBook(Long id, Book bookDetails) {
+        Book book = getBookById(id);
+        book.setTitle(bookDetails.getTitle());
+        book.setAuthor(bookDetails.getAuthor());
+        book.setIsbn(bookDetails.getIsbn());
+        book.setPrice(bookDetails.getPrice());
+        book.setPublishedDate(bookDetails.getPublishedDate());
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        Book book = getBookById(id);
+        bookRepository.delete(book);
+    }
+}
